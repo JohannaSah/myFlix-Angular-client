2da89aed216df2d3520c403780d79b1c5ddf87e7
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-04-05 17:25:25 +0200

    add: functionaity to spinner: resgistration&login

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,55 +1,53 @@
-// src/app/user-registration-form/user-registration-form.component.ts
+// import necessary modules and services
 import { Component, OnInit, Input } from '@angular/core';
-
-// You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-//
+// Declare the component and its metadata
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
   styleUrls: ['./user-registration-form.component.scss']
 })
 
-//
+// Declare the component class
 export class UserRegistrationFormComponent {
-  //
+  // Declare variables
+  public showSpinner = false;
   @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
-  //
+  // Inject services into the component
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar
   ) {}
 
-  //
+  // Implement the OnInit interface
   ngOnInit(): void {
   }
 
   // This is the function responsible for sending the form inputs to the backend
   registerUser(): void {
-    //
+    // Show spinner
+    this.showSpinner = true;
+    // Subscribe to the API registration method
     this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
-      //
       console.log(response);
-      //
+      // Close the dialog
       this.dialogRef.close();
-      //
+      // Display success message
       this.snackBar.open('User registration successful', 'OK', {
         duration: 2000
       });
-    }, (response) => {
-      this.snackBar.open('User registration successful', 'OK', {
-        duration: 2000
-      });
-    });
-  }
+    }, (error) => {
+      // Hide spinner
+      this.showSpinner = false;
+      // Display error message
+      this.snackBar.open('Registration unsuccessful', 'OK', { duration: 2000 });
+  });
+}
 
 }
