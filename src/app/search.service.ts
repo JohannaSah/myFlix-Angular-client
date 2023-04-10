b91657ae15eeef4ee79fb93986c3090c2c11420f
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-04-10 23:39:53 +0200

    refactored

--- src/app/search.service.ts
+++ src/app/search.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { BehaviorSubject } from 'rxjs';
 
 export interface Movie {
   _id: string;
